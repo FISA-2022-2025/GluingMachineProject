379c4681a0b0cbe60b32afacbf7fbecc9afcced6
Francky MARTINI <martini.francky@protonmail.com>
2024-02-09 10:29:43 +0100

    Ajout de boutons : previous et next image

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 import numpy as np
 import cv2
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QCheckBox, QLineEdit
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QCheckBox, QLineEdit, QPushButton
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+import glob
 
 class ApplicationWindow(QMainWindow):
     def __init__(self):
@@ -11,14 +12,49 @@ class ApplicationWindow(QMainWindow):
 
         # Charger l'image en niveaux de gris
         self.image = cv2.imread('images/capture0.jpg')
+        # Charger un array d'image à partir du dossier Images
+        self.images = [cv2.imread(file) for file in glob.glob('images/*.jpg')]
+        print(self.images.__len__())
 
-        if self.image is None:
-            print("Erreur lors du chargement de l'image. Veuillez vérifier le chemin.")
+        if self.images is None:
+            print("Erreur lors du chargement des images. Veuillez vérifier la présence d'image dans le chemin.")
             sys.exit()
 
         self.initUI()
 
+    # Fonction pour le calcul de la distance minimale entre deux contours
+    def find_min_distance(self, contours1, contours2):
+            """Calcul de la distance minimale entre deux ensembles de contours."""
+            min_dist = np.inf
+            for contour1 in contours1:
+                for contour2 in contours2:
+                    # Convertir les contours en numpy arrays pour le calcul de distance
+                    contour_array1 = np.squeeze(contour1)
+                    contour_array2 = np.squeeze(contour2)
+                    # Calculer toutes les distances entre les points des deux contours
+                    dist = np.sqrt(np.sum((contour_array1[:, None, :] - contour_array2[None, :, :]) ** 2, axis=2))
+                    # Trouver la distance minimale
+                    min_dist = min(min_dist, np.min(dist))
+            return min_dist
+
+    def nextImage(self):
+        """Afficher l'image suivante."""
+        if self.current_image_index < self.images.__len__():
+            self.image = self.images[self.current_image_index]
+            print(self.current_image_index)
+            self.current_image_index += 1
+            self.updateImage()
+
+    def previousImage(self):
+        """Afficher l'image précédente."""
+        if self.current_image_index > 0:
+            self.image = self.images[self.current_image_index]
+            print(self.current_image_index)
+            self.current_image_index -= 1
+            self.updateImage()
+
     def initUI(self):
+        """Initialiser l'interface utilisateur."""
         self.setWindowTitle('Projet encolleuse')
         self.setGeometry(100, 100, 800, 600)
 
@@ -28,6 +64,15 @@ class ApplicationWindow(QMainWindow):
 
         self.canvas = FigureCanvas(Figure(figsize=(5, 4)))
         layout.addWidget(self.canvas)
+
+        # Afficher des flèches pour passer d'une image à l'autre
+        self.current_image_index = 0
+        self.pushButtonPrevious = QPushButton("Précédent", self)
+        self.pushButtonPrevious.clicked.connect(self.previousImage)
+        layout.addWidget(self.pushButtonPrevious)
+        self.pushButtonNext = QPushButton("Suivant", self)
+        self.pushButtonNext.clicked.connect(self.nextImage)
+        layout.addWidget(self.pushButtonNext)
 
         # Créer deux axes dans le canevas : un pour l'image et un pour l'histogramme
         self.ax1, self.ax2 = self.canvas.figure.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]})
@@ -76,7 +121,8 @@ class ApplicationWindow(QMainWindow):
         layout.addWidget(self.checkBoxCollage)
 
 
-    def updateImage(self, state):
+    def updateImage(self):
+        """Mettre à jour l'image et l'histogramme en fonction des cases à cocher."""
         # Réinitialiser l'image filtrée à l'image originale
         image_filtree = self.image.copy()
         # Appliquer le filtre passe-bande
@@ -89,7 +135,7 @@ class ApplicationWindow(QMainWindow):
         if self.checkBoxLaplaceFilter.isChecked():
             # convertir l'image en niveaux de gris
             image_filtree = cv2.cvtColor(image_filtree, cv2.COLOR_BGR2GRAY)
-            image_filtree = cv2.Laplacian(image_filtree, cv2.CV_64F)          
+            image_filtree = cv2.Laplacian(image_filtree, cv2.CV_64F)
         # Appliquer la détection de contour
         if self.checkBoxCollage.isChecked() or self.checkBoxEdgeDetection.isChecked():
             x1, y1, x2, y2 = 100, 150, 300, 200  # Coordonnées de la zone de collage, TODO : interactif
@@ -100,13 +146,34 @@ class ApplicationWindow(QMainWindow):
                 masque[:] = [0, 0, 0]  # Rouge en BGR
 
                 # Appliquer une opération, comme la détection de contours, dans la zone sélectionnée
-                roi = cv2.Canny(image_filtree[y1:y2, x1:x2], 100, 200)
-                masque[y1:y2, x1:x2, 0] = roi  # Appliquer le contour en canal Bleu pour l'exemple
-                #masque[y1:y2, x1:x2, 1] = roi  # Appliquer le contour en canal Vert pour l'exemple
-                #masque[y1:y2, x1:x2, 2] = roi  # Conserver le rouge où il n'y a pas de contour
+                edges = cv2.Canny(image_filtree[y1:y2, x1:x2], 100, 200)
+                masque[y1:y2, x1:x2, 0] = edges  # Appliquer le contour en canal Bleu pour l'exemple
+                #masque[y1:y2, x1:x2, 1] = edges  # Appliquer le contour en canal Vert pour l'exemple
+                #masque[y1:y2, x1:x2, 2] = edges  # Conserver le rouge où il n'y a pas de contour
 
                 # Ajouter le masque à l'image filtrée
                 image_filtree = np.where(masque > 0, 255, image_filtree)  # Assigne une intensité élevée aux contours
+
+                # Extraire les contours de l'image filtrée
+                contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                # Ajuster les coordonnées des contours à la position réelle dans l'image
+                adjusted_contours = [contour + np.array([[x1, y1]]) for contour in contours]
+
+                # Calculer la distance entre les deux premiers contours trouvés
+                if len(contours) > 1:
+                    distance_px = self.find_min_distance(contours[0:1], contours[1:2])
+                    # TODO : convertir la distance en mm
+                    print(f"Distance minimale entre les deux premiers contours: {distance_px} pixels soit {distance_px*0.2:.2f} mm.")
+
+                    # Afficher les contours et la distance sur l'image
+                    cv2.drawContours(image_filtree, adjusted_contours, -1, (0, 255, 0), 3)
+                    self.ax1.clear()
+                    self.ax1.imshow(image_filtree, cmap='gray')
+                    self.ax1.axis('on')
+                    self.canvas.draw()
+                else:
+                    print("Moins de deux contours ont été détectés.")
             else:
                 # Si la détection de contour n'est pas sélectionnée, dessiner simplement un rectangle autour de la zone de collage
                 image_filtree = cv2.rectangle(image_filtree, (x1, y1), (x2, y2), (0, 255, 0), 2)
@@ -116,6 +183,12 @@ class ApplicationWindow(QMainWindow):
         self.ax1.clear()
         self.ax1.imshow(image_filtree, cmap='gray')
         self.ax1.axis('on')
+        # Afficher l'index de l'image et la distance entre les contours si elle existe sinon l'index de l'image
+        if self.checkBoxEdgeDetection.isChecked():
+            self.ax1.set_title(f"Image {self.current_image_index} - Distance entre les contours: {distance_px*0.2:.2f} mm")
+        else:
+            self.ax1.set_title(f"Image {self.current_image_index}")
+
 
         self.ax2.clear()
         self.ax2.hist(image_filtree.ravel(), bins=256, range=[0, 256], color='black')
@@ -124,6 +197,7 @@ class ApplicationWindow(QMainWindow):
         self.canvas.draw()
 
 if __name__ == '__main__':
+    """Exécuter l'application."""
     app = QApplication(sys.argv)
     ex = ApplicationWindow()
     ex.show()
