b2a1f905effb7739aae62bf7b48072405b8accdb
Francky MARTINI <martini.francky@protonmail.com>
2024-02-15 17:37:39 +0100

    V1

--- application_window.py
+++ application_window.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import cv2
 from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QCheckBox, QLineEdit, QPushButton, QComboBox
+from PySide6.QtGui import QIcon
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import glob
@@ -12,6 +13,7 @@ import time
 
 class ApplicationWindow(QMainWindow):
     def __init__(self):
+        '''Initialiser la fenêtre de l'application.'''
         super().__init__()
 
         # Charger l'image en niveaux de gris
@@ -39,21 +41,6 @@ class ApplicationWindow(QMainWindow):
 
         self.initUI()
 
-    # Fonction pour le calcul de la distance minimale entre deux contours
-    def find_min_distance(self, contours1, contours2):
-            """Calcul de la distance minimale entre deux ensembles de contours."""
-            min_dist = np.inf
-            for contour1 in contours1:
-                for contour2 in contours2:
-                    # Convertir les contours en numpy arrays pour le calcul de distance
-                    contour_array1 = np.squeeze(contour1)
-                    contour_array2 = np.squeeze(contour2)
-                    # Calculer toutes les distances entre les points des deux contours
-                    dist = np.sqrt(np.sum((contour_array1[:, None, :] - contour_array2[None, :, :]) ** 2, axis=2))
-                    # Trouver la distance minimale
-                    min_dist = min(min_dist, np.min(dist))
-            return min_dist
-
     def nextImage(self):
         """Afficher l'image suivante."""
         if self.current_image_index < self.images.__len__():
@@ -74,6 +61,8 @@ class ApplicationWindow(QMainWindow):
         """Initialiser l'interface utilisateur."""
         self.setWindowTitle('Projet encolleuse')
         self.setGeometry(100, 100, 800, 600)
+        # Changer l'icon de la fenêtre
+        self.setWindowIcon(QIcon('images/icon.png'))
 
         centralWidget = QWidget(self)
         self.setCentralWidget(centralWidget)
@@ -87,7 +76,9 @@ class ApplicationWindow(QMainWindow):
         displayPanelLayout = QVBoxLayout()
         layout.addLayout(displayPanelLayout, 3)
 
+        # Créer un canevas pour afficher l'image et l'histogramme
         self.canvas = FigureCanvas(Figure(figsize=(5, 4)))
+        self.canvas.figure.set_facecolor('#31363b')
         displayPanelLayout.addWidget(self.canvas)
 
         # Afficher un selecteur entre vidéo direct et image statique
@@ -107,13 +98,37 @@ class ApplicationWindow(QMainWindow):
         controlPanelLayout.addWidget(self.pushButtonNext)
 
         # Créer deux axes horizontal dans le canevas : un pour l'image et un pour l'histogramme horizontal
-        self.ax1, self.ax2 = self.canvas.figure.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]})
+        self.ax1, self.ax2, self.ax3 = self.canvas.figure.subplots(3, 1, gridspec_kw={'height_ratios': [2, 1, 1]})
 
+        # Afficher l'image et l'histogramme (ax1: image, ax2: histogramme, ax3: texte sur 5 lignes)
+        # ax1
         self.ax1.imshow(self.image, cmap='gray')  # Afficher l'image en niveaux de gris par défaut
         self.ax1.axis('on')  # Cacher les axes pour l'image
+        self.ax1.tick_params(axis='x', colors='#1de9b6')  # Couleur des graduations en x
+        self.ax1.tick_params(axis='y', colors='#1de9b6')  # Couleur des graduations en y
+        self.ax1.spines['bottom'].set_color('#1de9b6') # Couleur de l'image
+        self.ax1.spines['top'].set_color('#1de9b6')
+        self.ax1.spines['right'].set_color('#1de9b6')
+        self.ax1.spines['left'].set_color('#1de9b6')
+        # ax2
+        self.ax2.set_facecolor('#31363b')  # Couleur de fond de l'histogramme
+        self.ax2.tick_params(axis='x', colors='#1de9b6')  # Couleur des graduations en x
+        self.ax2.tick_params(axis='y', colors='#1de9b6')  # Couleur des graduations en y
+        self.ax2.spines['bottom'].set_color('#1de9b6')# couleur de l'histogramme
+        self.ax2.spines['top'].set_color('#1de9b6')
+        self.ax2.spines['right'].set_color('#1de9b6')
+        self.ax2.spines['left'].set_color('#1de9b6')
+
+        # Afficher le texte dans ax3
+        self.ax3.axis('off')
+        self.ax3.text(0, 0.8, "Contour gauche : ", ha='left', va='center', fontsize=14, color='#1de9b6')
+        self.ax3.text(0, 0.6, "Contour droite : ", ha='left', va='center', fontsize=14, color='#1de9b6')
+        self.ax3.text(0, 0.4, "Contour bas    : ", ha='left', va='center', fontsize=14, color='#1de9b6')
+        self.ax3.text(0, 0.2, "Contour haut   : ", ha='left', va='center', fontsize=14, color='#1de9b6')
+
 
         # Afficher l'histogramme initial
-        self.ax2.hist(self.image.ravel(), bins=256, range=[0,256], color='black')
+        self.ax2.hist(self.image.ravel(), bins=256, range=[0,256], color='#1de9b6')
         self.ax2.set_xlim([0, 256])
 
         # Ajouter une case à cocher pour le filtre passe-bande
@@ -168,7 +183,7 @@ class ApplicationWindow(QMainWindow):
         controlPanelLayout.addWidget(self.pushButtonInitPlateau)
 
         # Ajout d'un bouton pour le lancement du gcode
-        self.pushButtonGcode = QPushButton("Lancer Gcode", self)
+        self.pushButtonGcode = QPushButton("Lancer le gcode", self)
         self.pushButtonGcode.clicked.connect(self.lancerGcode)
         controlPanelLayout.addWidget(self.pushButtonGcode)
 
@@ -193,8 +208,10 @@ class ApplicationWindow(QMainWindow):
         # Fermer le thread s'il est en cours d'exécution
         if self.cam_cleaner is not None:
             self.cam_cleaner.stop()
-        self.cap.release()
-        self.arduino.close()
+        if self.arduino is not None:
+            self.arduino.close()
+        if self.cap is not None:
+            self.cap.release()
         self.close()
 
     def initOctoPrint(self):
@@ -220,6 +237,7 @@ class ApplicationWindow(QMainWindow):
         octo_client.send_gcode_command(['G1 X117.00 Y250 Z20 F2000'])
 
     def valider(self):
+        '''Envoyer la commande de validation au bras robot'''
         # Déplacer le plateau
         self.deplacerPlateau()
         # Attendre 1 demi seconde
@@ -229,6 +247,7 @@ class ApplicationWindow(QMainWindow):
         print("Valider")
 
     def rebus(self):
+        '''Envoyer la commande de rebus au bras robot'''
         # Déplacer le plateau
         self.deplacerPlateau()
         # Attendre 1 demi seconde
@@ -244,10 +263,11 @@ class ApplicationWindow(QMainWindow):
             if self.cap is not None:
                 self.cap.release()
             self.image = self.images[self.current_image_index]
+            # redimensionner l'image pour l'affichage
+            self.image = cv2.resize(self.image, (800, 500))
             self.updateImage()
             print("Image statique")
         elif text == "Vidéo direct":
-            # Vérifier si la caméra est ouverte
             # Initialiser la caméra
             try:
                 self.cap = cv2.VideoCapture('http://10.167.50.3/webcam/?action=stream')
@@ -267,6 +287,97 @@ class ApplicationWindow(QMainWindow):
                 # Rebasculer sur l'image statique
                 self.comboBox.setCurrentIndex(0)
 
+    def detecterContours(self, image):
+        '''
+        Détecter les contours dans une image
+        '''
+        # Définir les zones de détection de contour dans l'image
+        if self.comboBox.currentText() == "Image statique":
+            zones = [
+                #       x1   y1   x2   y2
+                (70, 75, 100, 170),  # Zone gauche
+                (320, 75, 360, 180),  # Zone droite
+                (110, 55, 300, 80),  # Zone haut
+                (110, 170, 300, 220)   # Zone bas
+            ]
+        elif self.comboBox.currentText() == "Vidéo direct":
+            zones = [
+            #       x1   y1   x2   y2
+                   (230, 290, 260, 370),  # Zone gauche
+                   (280, 380, 490, 405),  # Zone droite
+                   (500, 290, 520, 370),  # Zone haut
+                   (280, 245, 490, 270)   # Zone bas
+               ]
+
+       # Appliquer la détection de contour uniquement dans les zones sélectionnées
+        if self.checkBoxEdgeDetection.isChecked():
+
+            distance_px = [0, 0, 0, 0]
+
+            for zone in zones:
+                x1, y1, x2, y2 = zone
+                # Appliquer un filtre laplacien pour accentuer les contours
+                edges = cv2.Canny(image[y1:y2, x1:x2], 100, 200, 5, L2gradient=True)
+
+                # Extraire les contours de l'image filtrée pour chaque zone
+                contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                # Ajuster les coordonnées des contours à la position réelle dans l'image
+                adjusted_contours = [contour + np.array([[x1, y1]]) for contour in contours]
+
+                # Afficher les contours sur l'image
+                cv2.drawContours(image, adjusted_contours, -1, (29, 233, 182), 3)
+
+                # Calculer la distance minimale entre les contours
+                zone_index = zones.index(zone)
+                distance_px[zone_index] = self.find_min_distance(contours[0:1], contours[1:2])
+
+            # Dessiner simplement un rectangle autour de chaque zone de collage
+            for zone in zones:
+               x1, y1, x2, y2 = zone
+               cv2.rectangle(image, (x1, y1), (x2, y2), (29, 233, 182), 2)
+
+            print(distance_px)
+
+            # Mettre à jour l'affichage
+            self.ax3.text(0, 0.8, f"Contour gauche : {distance_px[0]*0.2:.2f} mm.", ha='left', va='center', fontsize=14, color='#1de9b6')
+            self.ax3.text(0, 0.6, f"Contour droite : {distance_px[1]*0.2:.2f} mm.", ha='left', va='center', fontsize=14, color='#1de9b6')
+            self.ax3.text(0, 0.4, f"Contour bas    : {distance_px[2]*0.2:.2f} mm.", ha='left', va='center', fontsize=14, color='#1de9b6')
+            self.ax3.text(0, 0.2, f"Contour haut   : {distance_px[3]*0.2:.2f} mm.", ha='left', va='center', fontsize=14, color='#1de9b6')
+
+            # Mettre à jour l'affichage
+            self.ax1.clear()
+            self.ax1.imshow(image, cmap='gray')
+            self.ax1.axis('on')
+            self.canvas.draw()
+        else:
+           # Dessiner simplement un rectangle autour de chaque zone de collage
+           for zone in zones:
+               x1, y1, x2, y2 = zone
+               cv2.rectangle(image, (x1, y1), (x2, y2), (29, 233, 182), 2)
+
+           # Mettre à jour l'affichage
+           self.ax1.clear()
+           self.ax1.imshow(image, cmap='gray')
+           self.ax1.axis('on')
+           self.canvas.draw()
+
+    # Fonction pour le calcul de la distance minimale entre deux contours
+    def find_min_distance(self, contours1, contours2):
+           """Calcul de la distance minimale entre deux ensembles de contours."""
+           min_dist = np.inf
+           for contour1 in contours1:
+               for contour2 in contours2:
+                   # Convertir les contours en numpy arrays pour le calcul de distance
+                   contour_array1 = np.squeeze(contour1)
+                   contour_array2 = np.squeeze(contour2)
+                   # Calculer toutes les distances entre les points des deux contours
+                   # distance = sqrt((x1 - x2)^2 + (y1 - y2)^2)
+                   dist = np.sqrt(np.sum((contour_array1[:, None, :] - contour_array2[None, :, :]) ** 2, axis=2))
+                   # Trouver la distance minimale
+                   min_dist = min(min_dist, np.min(dist))
+           return min_dist
+
     def updateImage(self):
         """Mettre à jour l'image et l'histogramme en fonction des cases à cocher."""
         # Réinitialiser l'image filtrée à l'image originale
@@ -283,69 +394,23 @@ class ApplicationWindow(QMainWindow):
             image_filtree = cv2.cvtColor(image_filtree, cv2.COLOR_BGR2GRAY)
             image_filtree = cv2.Laplacian(image_filtree, cv2.CV_64F)
         # Appliquer la détection de contour
-        if self.checkBoxCollage.isChecked() or self.checkBoxEdgeDetection.isChecked():            
-            # zone 1 : 230, 290, 260, 370
-            # zone 2 : 280, 380, 490, 405
-            # zone 3 : 500, 290, 520, 370
-            # zone 4 : 280, 245, 490, 280
-            x1, y1, x2, y2 = 280, 255, 490, 270  # Coordonnées de la zone de collage, TODO : interactif
-            # Appliquer la détection de contour uniquement dans la zone sélectionnée
-            if self.checkBoxEdgeDetection.isChecked():
-                # Créer un masque rouge
-                masque = np.zeros_like(image_filtree)
-                masque[:] = [0, 0, 0]  # Rouge en BGR
-
-                # Appliquer une opération, comme la détection de contours, dans la zone sélectionnée
-                edges = cv2.Canny(image_filtree[y1:y2, x1:x2], 100, 200)
-                masque[y1:y2, x1:x2, 0] = edges  # Appliquer le contour en canal Bleu pour l'exemple
-                #masque[y1:y2, x1:x2, 1] = edges  # Appliquer le contour en canal Vert pour l'exemple
-                #masque[y1:y2, x1:x2, 2] = edges  # Conserver le rouge où il n'y a pas de contour
-
-                # Ajouter le masque à l'image filtrée
-                image_filtree = np.where(masque > 0, 255, image_filtree)  # Assigne une intensité élevée aux contours
-
-                # Extraire les contours de l'image filtrée
-                contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-                # Ajuster les coordonnées des contours à la position réelle dans l'image
-                adjusted_contours = [contour + np.array([[x1, y1]]) for contour in contours]
-
-                # Calculer la distance entre les deux premiers contours trouvés
-                distance_px = None
-                if len(contours) > 1:
-                    try:
-                        distance_px = self.find_min_distance(contours[0:1], contours[1:2])
-                        # TODO : convertir la distance en mm
-                        print(f"Distance minimale entre les deux premiers contours: {distance_px} pixels soit {distance_px*0.2:.2f} mm.")
-
-                        # Afficher les contours et la distance sur l'image
-                        cv2.drawContours(image_filtree, adjusted_contours, -1, (0, 255, 0), 3)
-                        self.ax1.clear()
-                        self.ax1.imshow(image_filtree, cmap='gray')
-                        self.ax1.axis('on')
-                        self.canvas.draw()
-                    except:
-                        print("Erreur lors du calcul de la distance entre les contours.")
-                else:
-                    print("Moins de deux contours ont été détectés.")
-            else:
-                # Si la détection de contour n'est pas sélectionnée, dessiner simplement un rectangle autour de la zone de collage
-                image_filtree = cv2.rectangle(image_filtree, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
+        if self.checkBoxCollage.isChecked() or self.checkBoxEdgeDetection.isChecked():
+            self.detecterContours(image_filtree)
 
         # Mettre à jour l'image et l'histogramme
         self.ax1.clear()
         self.ax1.imshow(image_filtree, cmap='gray')
         self.ax1.axis('on')
         # Afficher l'index de l'image et la distance entre les contours si elle existe sinon l'index de l'image
-        if self.checkBoxEdgeDetection.isChecked() and distance_px is not None:
-            self.ax1.set_title(f"Image {self.current_image_index} - Distance entre les contours: {distance_px*0.2:.2f} mm")
-        else:
-            self.ax1.set_title(f"Image {self.current_image_index}")
+        if self.comboBox.currentText() == "Image statique":
+            self.ax1.set_title(f"Image {self.current_image_index}", color='#1de9b6')
+
+        elif self.comboBox.currentText() == "Vidéo direct":
+            self.ax1.set_title(f"Image en direct", color='#1de9b6')
 
 
         self.ax2.clear()
-        self.ax2.hist(image_filtree.ravel(), bins=256, range=[0, 256], color='black')
+        self.ax2.hist(image_filtree.ravel(), bins=256, range=[0, 256], color='#1de9b6')
         self.ax2.set_xlim([0, 256])
         # Mettre à jour le canevas
         self.canvas.draw()
